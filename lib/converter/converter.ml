9747bf0c3b699688423ac331f435c22afa7fb579
Gerrit Proessl <gerrit.proessl@gmail.com>
2020-11-29 12:11:14 +0100

    cleanup converter

--- lib/converter/converter.ml
+++ lib/converter/converter.ml
@@ -18,34 +18,56 @@ let init () =
 
 (* the state is global unlike the namespaces which are scope dependent *)
 module State = struct
-    type t = 
-        { mutable tys : int_struct_map
-        ; mutable fns : int_fn_map
-        }
 
-    type string_map = (string, t, String.comparator_witness) Map.t
+    let  struct_map : ((int, int_struct_map, Int.comparator_witness) Map.t) ref = ref (Map.empty (module Int))
+    let fn_map : ((int, int_fn_map, Int.comparator_witness) Map.t) ref = ref (Map.empty (module Int))
 
     let cid = ref 0x1000 (* all prior symbols are reserved *)
 
-    let empty () : t = 
-        { tys = Map.empty (module Int)
-        ; fns = Map.empty (module Int)
-        }
+    let clear () : unit = 
+        struct_map := Map.empty (module Int)
+        ; fn_map := Map.empty (module Int)
 
-    let clear (s : t) : unit = 
-        s.tys <- Map.empty (module Int)
-        ; s.fns <- Map.empty (module Int)
+    let get_tys (mid : int) : int_struct_map =
+        Map.find_exn !struct_map mid
 
-    let add_ty (s : t) (ty : I.Struct.t) : unit =
-        s.tys <- Map.add_exn s.tys ~key:ty.id ~data:ty
+    let get_fns (mid : int) : int_fn_map =
+        Map.find_exn !fn_map mid
 
-    let add_fn (s : t) (fn : I.Fn.t) : unit = 
-        s.fns <- Map.add_exn s.fns ~key:fn.id ~data:fn
+    let add_ty (mid : int) (ty : I.Struct.t) : unit =
+        struct_map := Map.update !struct_map mid ~f:(fun ms -> 
+            match ms with
+                | None   -> Map.singleton (module Int) ty.id ty
+                | Some s -> Map.add_exn s ~key:ty.id ~data:ty
+            )
+
+    let get_ty (mid : int) (sid : int) : I.Struct.t =
+        Map.find_exn (get_tys mid) sid
+
+    let add_fn (mid : int) (fn : I.Fn.t) : unit = 
+        fn_map := Map.update !fn_map mid ~f:(fun ms -> 
+            match ms with
+                | None   -> Map.singleton (module Int) fn.id fn
+                | Some s -> Map.add_exn s ~key:fn.id ~data:fn
+            )
+
+    let get_fn (mid : int) (fid : int) : I.Fn.t =
+        Map.find_exn (get_fns mid) fid
+
+    let search_ty (sid : int) : I.Struct.t = (* this function should probably take an error argument *)
+        Map.fold !struct_map ~init:None ~f:(fun ~key:_ ~data:d a -> 
+            match a with
+                | None -> Map.find d sid
+                | Some a -> Some a) |> Option.value_exn
+
+    let search_fn (fid : int) : I.Fn.t = (* this function should probably take an error argument *)
+        Map.fold !fn_map ~init:None ~f:(fun ~key:_ ~data:d a -> 
+            match a with
+                | None -> Map.find d fid
+                | Some a -> Some a) |> Option.value_exn
 
     let next_id () : int = let ii = !cid in cid := !cid + 1; ii
 
-    let get_map (ms : A.module_t list) : string_map =
-        List.map ms ~f:(fun m -> (m.name, empty ())) |> Map.of_alist_exn (module String)
 end
 
 
@@ -91,20 +113,20 @@ let rec get_ty (ns : string_type_map) (t : A.type_t) : I.Type.t =
         | A.Type_fn (ts, t) -> I.Type.Fn (List.map ts ~f:(get_ty ns), get_ty ns t)
 
 (* the next two functions basically do the same but the first is for structs inside functions *)
-let convert_struct (st : State.t) (s : A.struct_t) (ty_ns : string_type_map) : string_type_map =
+let convert_struct (mid : int) (s : A.struct_t) (ty_ns : string_type_map) : string_type_map =
     let ty_ns = add_ty_ns s ty_ns in
     let sns = Map.find_exn ty_ns s.sname in
-    State.add_ty st (({ id = sns.id
+    State.add_ty mid (({ id = sns.id
                    ; fs = (List.map s.fs 
                                     ~f:(fun fd -> ({ id = Map.find_exn sns.fs fd.name
                                                    ; ty = (get_ty ty_ns fd.ty )
                                                    } : I.Field.t)))
                          }) : I.Struct.t) ; ty_ns
 
-let convert_structs (st : State.t) (m : A.module_t) (ty_ns : string_type_map) : unit = 
+let convert_structs (mid : int) (m : A.module_t) (ty_ns : string_type_map) : unit = 
     iter_structs m (fun s -> 
         let sns = Map.find_exn ty_ns s.sname in
-        State.add_ty st (({ id = sns.id
+        State.add_ty mid (({ id = sns.id
                        ; fs = (List.map s.fs 
                                         ~f:(fun fd -> ({ id = Map.find_exn sns.fs fd.name
                                                        ; ty = (get_ty ty_ns fd.ty )
@@ -135,7 +157,7 @@ let get_args_ns (args : A.field_t list) (ty_ns : string_type_map) : string_var_m
                                                         ; ty = get_ty ty_ns a.ty
                                                         })
 
-let rec get_exp (st : State.t) (e : A.exp_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) (var_ns : string_var_map) (structs : int_struct_map)
+let rec get_exp (mid : int) (e : A.exp_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) (var_ns : string_var_map)
     : I.Exp.t =
     let rec flat_app = begin function 
         | A.Exp_app (l, r) -> r :: (flat_app l) 
@@ -155,7 +177,7 @@ let rec get_exp (st : State.t) (e : A.exp_t) (ty_ns : string_type_map) (fn_ns : 
                                       let (fty, ftyr) = f.ty
                                       in I.Exp.Ref (f.id, I.Type.Fn (fty, ftyr))
         | A.Exp_call e        -> 
-            let le = get_exp st e ty_ns fn_ns var_ns structs in
+            let le = get_exp mid e ty_ns fn_ns var_ns in
             let t = I.Exp.get_type le in
             (match t with
                 | I.Type.Fn ([], r) -> I.Exp.App (le, [], r) (* TODO: better error for not enough arguments *)
@@ -166,19 +188,19 @@ let rec get_exp (st : State.t) (e : A.exp_t) (ty_ns : string_type_map) (fn_ns : 
                     | [] -> raise (Std.Error.Comp "type that should be present not found")
                     | x :: _ -> Map.find_exn x.fs s
             in
-            let le = get_exp st l ty_ns fn_ns var_ns structs in
+            let le = get_exp mid l ty_ns fn_ns var_ns in
             let lt = I.Exp.get_type le in
             (match lt with
                 | I.Type.Fn _   -> raise (Std.Error.Err "unable to access member of function expression")
                 | I.Type.Prim t -> 
                     let mem = find_ty_by_int t r in
-                    let s = Option.value (Map.find st.tys t) ~default:(Map.find_exn structs t)  in
+                    let s = State.search_ty mid in
                     let mem_f = List.find_exn s.fs ~f:(fun a -> Int.equal a.id mem) in
                     I.Exp.Access (le, mem, mem_f.ty))
         | A.Exp_app (l, r)    -> 
             match flat_app (A.Exp_app (l, r)) |> List.rev with | [] -> assert false | x :: xs ->
-                let le = get_exp st x ty_ns fn_ns var_ns structs in 
-                let res = List.map xs ~f:(fun a -> get_exp st a ty_ns fn_ns var_ns structs) in
+                let le = get_exp mid x ty_ns fn_ns var_ns in 
+                let res = List.map xs ~f:(fun a -> get_exp mid a ty_ns fn_ns var_ns ) in
                 let lt = I.Exp.get_type le in
                 let rts = List.map res ~f:(fun a -> I.Exp.get_type a) in
                 match lt with
@@ -191,23 +213,22 @@ let rec get_exp (st : State.t) (e : A.exp_t) (ty_ns : string_type_map) (fn_ns : 
                              else I.Exp.App (le, res, fr)
 
 
-let get_var (st : State.t) (v : A.stm_var_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) (var_ns : string_var_map) (structs : int_struct_map)
+let get_var (mid : int) (v : A.stm_var_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) (var_ns : string_var_map)
         : I.Stm.var =
     let lt = get_ty ty_ns v.ty in
     let ni = State.next_id () in
     { id = ni
     ; ty = lt
-    ; v = get_exp st v.v ty_ns fn_ns var_ns structs
+    ; v = get_exp mid v.v ty_ns fn_ns var_ns
     }
 
 let rec get_stm 
-        (st : State.t)
+        (mid : int)
         (s : A.stm_t) 
         (ty_ns : string_type_map) 
         (fn_ns : string_fn_map) 
         (var_ns : string_var_map) 
         (label_ns : string_int_map)
-        (structs : int_struct_map)
         : ((I.Stm.t list) * string_var_map * string_type_map * string_fn_map) =
     match s with
         | A.Stm_scope (mn, vs, me, ss) -> 
@@ -216,51 +237,50 @@ let rec get_stm
             let (vss, nvar_ns) = List.fold vs ~init:([], var_ns) 
                                    ~f:(fun acc a ->
                                         let (vs, var_ns) = acc in
-                                        let v = get_var st a ty_ns fn_ns var_ns structs in
+                                        let v = get_var mid a ty_ns fn_ns var_ns in
                                         let nvar_ns = Map.update var_ns a.name ~f:(fun _ -> { id = v.id; ty = v.ty } ) in
                                         (v :: vs, nvar_ns))
             in
-            let ne = Option.map me ~f:(fun a -> get_exp st a ty_ns fn_ns nvar_ns structs) in
-            let nss = get_stm_list st ss ty_ns fn_ns nvar_ns nlabel_ns structs in
+            let ne = Option.map me ~f:(fun a -> get_exp mid a ty_ns fn_ns nvar_ns ) in
+            let nss = get_stm_list mid ss ty_ns fn_ns nvar_ns nlabel_ns in
             ([I.Stm.Scope (ni, List.rev vss, ne, nss)], var_ns, ty_ns, fn_ns)
         | A.Stm_let v -> 
             let lt = get_ty ty_ns v.ty in
             let ni = State.next_id () in
             let nvar_ns = Map.update var_ns v.name ~f:(fun _ -> {id = ni; ty = lt }) in
-            ([I.Stm.Let ({ id = ni; ty = lt; v = get_exp st v.v ty_ns fn_ns var_ns structs})], nvar_ns, ty_ns, fn_ns)
-        | A.Stm_exp e         -> ([I.Stm.Exp (get_exp st e ty_ns fn_ns var_ns structs)], var_ns, ty_ns, fn_ns)
-        | A.Stm_return me     -> ([I.Stm.Return (Option.map me ~f:(fun a -> get_exp st a ty_ns fn_ns var_ns structs))], var_ns, ty_ns, fn_ns)
+            ([I.Stm.Let ({ id = ni; ty = lt; v = get_exp mid v.v ty_ns fn_ns var_ns })], nvar_ns, ty_ns, fn_ns)
+        | A.Stm_exp e         -> ([I.Stm.Exp (get_exp mid e ty_ns fn_ns var_ns )], var_ns, ty_ns, fn_ns)
+        | A.Stm_return me     -> ([I.Stm.Return (Option.map me ~f:(fun a -> get_exp mid a ty_ns fn_ns var_ns ))], var_ns, ty_ns, fn_ns)
         | A.Stm_jump mn       -> 
             let v = Map.find_exn label_ns (Option.value mn ~default:"__label") in
             ([I.Stm.Jump v], var_ns, ty_ns, fn_ns)
         | A.Stm_break mn       -> 
             let v = Map.find_exn label_ns (Option.value mn ~default:"__label") in
             ([I.Stm.Break v], var_ns, ty_ns, fn_ns)
-        | A.Stm_struct s -> ([], var_ns, convert_struct st s ty_ns, fn_ns)
-        | A.Stm_fn f     -> ([], var_ns, ty_ns, convert_fn st f ty_ns fn_ns structs)
+        | A.Stm_struct s -> ([], var_ns, convert_struct mid s ty_ns, fn_ns)
+        | A.Stm_fn f     -> ([], var_ns, ty_ns, convert_fn mid f ty_ns fn_ns)
 
 and get_stm_list 
-        (st : State.t)
+        (mid : int)
         (ss: A.stm_t list)
         (ty_ns  : string_type_map)
         (fn_ns : string_fn_map)
         (var_ns : string_var_map)
         (label_ns : string_int_map)
-        (structs : int_struct_map)
         : I.Stm.t list =
     let (r, _, _, _) = List.fold ss ~init:([], var_ns, ty_ns, fn_ns) (* TODO: maybe fold other way *)
                               ~f:(fun acc a ->
                                     let (ss, var_ns, ty_ns, fn_ns) = acc in
-                                    let (s, nvar_ns, nty_ns, nfn_ns) = (get_stm st a ty_ns fn_ns var_ns label_ns structs) in
+                                    let (s, nvar_ns, nty_ns, nfn_ns) = (get_stm mid a ty_ns fn_ns var_ns label_ns ) in
                                     (List.concat [s; ss], nvar_ns, nty_ns, nfn_ns))
     in (List.rev r)
 
-and convert_fn (st : State.t) (f : A.fn_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) (structs : int_struct_map): string_fn_map =
+and convert_fn (mid : int) (f : A.fn_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) : string_fn_map =
     let fn_ns = add_fn_ns f fn_ns ty_ns in
     let fns = Map.find_exn fn_ns f.fname in
     let var_ns = get_args_ns f.args ty_ns in
-    let stms = get_stm_list st f.stms ty_ns fn_ns var_ns (Map.empty (module String)) structs in
-    State.add_fn st ({ id = fns.id
+    let stms = get_stm_list mid f.stms ty_ns fn_ns var_ns (Map.empty (module String)) in
+    State.add_fn mid ({ id = fns.id
                          ; args = List.map f.args ~f:(fun a -> get_field a var_ns)
                          ; ty = (let (_, r) = fns.ty in r)
                          ; stms = stms
@@ -269,12 +289,12 @@ and convert_fn (st : State.t) (f : A.fn_t) (ty_ns : string_type_map) (fn_ns : st
                          } : I.Fn.t); fn_ns
 
 
-let convert_fns (st : State.t) (m : A.module_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) (structs : int_struct_map): unit =
+let convert_fns (mid : int) (m : A.module_t) (ty_ns : string_type_map) (fn_ns : string_fn_map) : unit =
     iter_fns m (fun f -> 
         let fns = Map.find_exn fn_ns f.fname in
         let var_ns = get_args_ns f.args ty_ns in
-        let stms = get_stm_list st f.stms ty_ns fn_ns var_ns (Map.empty (module String)) structs in
-        State.add_fn st ({ id = fns.id
+        let stms = get_stm_list mid f.stms ty_ns fn_ns var_ns (Map.empty (module String)) in
+        State.add_fn mid ({ id = fns.id
                              ; args = List.map f.args ~f:(fun a -> get_field a var_ns)
                              ; ty = (let (_, r) = fns.ty in r)
                              ; stms = stms
@@ -292,28 +312,24 @@ let includes (xs : (int * ('a, 'v, 'cmp) Map.t) list) (m : A.module_t) (mod_ns :
     Std.Map.merge_list comp fss (fun ~key:_ a _ -> a) 
 
 let convert (ms : A.module_t list) : I.Unit.t = (* this is all so inefficient *)
-    let st_map = State.get_map ms in
     let mod_ns = get_mod_ns ms in
     let get_mid k = Map.find_exn mod_ns k in
     let ty_nss = prim_ty_ns :: (List.map ~f:(fun m -> (get_mid m.name, get_ty_ns m)) ms) in
-    let () = List.iter ~f:(fun m -> convert_structs (Map.find_exn st_map m.name)  m (includes ty_nss m mod_ns (module String))) ms in
+    let () = List.iter ~f:(fun m -> convert_structs (get_mid m.name)  m (includes ty_nss m mod_ns (module String))) ms in
     let fn_nss = prim_fn_ns :: (List.map ~f:(fun m -> (get_mid m.name, get_fn_ns m (includes ty_nss m mod_ns (module String)))) ms) in
-    let () = List.iter ~f:(fun m -> (convert_fns (Map.find_exn st_map m.name) m
+    let () = List.iter ~f:(fun m -> (convert_fns (get_mid m.name) m
                                 (includes ty_nss m mod_ns (module String)) 
-                                (includes fn_nss m mod_ns (module String)) 
-                                (includes (List.map ~f:(fun (i, sm) -> (get_mid i, sm.tys)) (Map.to_alist st_map)) m mod_ns (module Int))))
+                                (includes fn_nss m mod_ns (module String))))
                                 ms
     in
-    let struct_map = Map.map st_map ~f:(fun d -> d.tys) in
-    let fn_map = Map.map st_map ~f:(fun d -> d.fns) in
     let mods = (List.fold ms ~init:(Map.empty (module Int)) 
                            ~f:(fun acc m -> 
                             let mid = get_mid m.name in
                             Map.add_exn acc ~key:mid
                                             ~data:({ file = m.file
                                                    ; id = mid
-                                                   ; fs = Map.find_exn fn_map m.name
-                                                   ; ds = Map.find_exn struct_map m.name
+                                                   ; fs = State.get_fns mid
+                                                   ; ds = State.get_tys mid
                                                    ; incs = List.map m.incs ~f:get_mid
                                                    } : I.Module.t)))
     in
